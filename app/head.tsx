86b2e079ef567300f67daefb277dab199ab81627
Celestiq <aayush.sharma@iitg.ac.in>
2025-09-13 11:03:00 +0530

    updated fonts

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&family=Playfair+Display:wght@700&display=swap"
+        rel="stylesheet"
+      />
+    </>
+  );
+}
