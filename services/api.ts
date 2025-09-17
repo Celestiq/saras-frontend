c37acbb7d0e55c519ea1db7c154db40345acc1db
Celestiq <aayush.sharma@iitg.ac.in>
2025-09-17 09:22:14 +0530

    minor api changes

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 // frontend/services/api.ts
 
-const API_PREFIX = "http://0.0.0.0:8080";
-// const API_PREFIX = "https://api.mysaras.club";
+// const API_PREFIX = "http://0.0.0.0:8080";
+const API_PREFIX = "https://api.mysaras.club";
 
 // --- Authentication ---
 
